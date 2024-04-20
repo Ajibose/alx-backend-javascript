df24f95a9a23a9c805a37bcac93ceb40358d0eb4
Ajibose <ajiboseibrahim12@gmail.com>
2024-04-20 14:24:46 +0000

    Task5

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,20 @@
+export default class Building {
+  constructor(sqft) {
+    this.sqft = sqft;
+
+    if (this.constructor !== Building && this.evacuationWarningMessage === undefined) {
+    throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
+  }
+
+  set sqft(value) {
+    if (typeof value !== 'number') {
+      throw new Error('Area must be a number');
+    }
+    this._sqft = value;
+  }
+  
+  get sqft() {
+    return this._sqft;
+  }
+}
