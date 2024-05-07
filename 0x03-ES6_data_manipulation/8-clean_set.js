d7a1bb3ef1185ed744997571a04635b3d51333a2
Ajibose <ajiboseibrahim12@gmail.com>
2024-05-07 11:27:54 +0000

    Task8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -2,7 +2,7 @@ export default function cleanSet(set, startString) {
   let resultString = '';
   const idx = startString.length;
   for (const element of set) {
-    if (element.startsWith(startString) && startString !== '') {
+    if (element.startsWith(startString) && Boolean(startString)) {
       resultString += `${element.slice(idx)}-`;
     }
   }
