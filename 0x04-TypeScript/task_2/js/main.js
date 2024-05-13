5fca54395b07f40fd493cfe030492c556a578a45
Ajibose <ajiboseibrahim12@gmail.com>
2024-05-13 10:38:41 +0000

    Task_2 dir

---
+++ 0x04-TypeScript/task_2/js/main.js
@@ -0,0 +1,30 @@
+class Director {
+    workFromHome() {
+        return 'Working from home';
+    }
+    getCoffeeBreak() {
+        return 'Getting a coffee break';
+    }
+    workDirectorTasks() {
+        return 'Getting to director tasks';
+    }
+}
+class Teacher {
+    workFromHome() {
+        return 'Cannot work from home';
+    }
+    getCoffeeBreak() {
+        return 'Cannot have a break';
+    }
+    workTeacherTasks() {
+        return 'Getting to work';
+    }
+}
+function createEmployee(salary) {
+    if (typeof salary === 'number' && salary < 500) {
+        return new Teacher();
+    }
+    else {
+        return new Director();
+    }
+}
