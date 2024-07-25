05b5631f2e5ad626e2726e739c58c532f6a00cf2
Ajibose Ibrahim <ajiboseibrahim12@gmail.com>
2024-07-25 16:19:39 +0100

    Task2

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,27 @@
+const fs = require('node:fs');
+
+function countStudents(path) {
+  try {
+    const contents = fs.readFileSync(path, 'utf8');
+    const fields = {};
+    const contentList = contents.trimEnd().split('\n').slice(1);
+    for (const content of contentList) {
+      const [firstName, , , field] = content.split(',');
+      if (!(field in fields)) {
+        fields[field] = [];
+      }
+      fields[field].push(firstName);
+    }
+    const noOfStudents = contentList.length;
+    console.log(`Number of students: ${noOfStudents}`);
+    Object.entries(fields).forEach(([key, value]) => {
+      console.log(`Number of students in ${key}: ${value.length}. List: ${value.join(', ')}`);
+    });
+  } catch (err) {
+    if (err.code === 'ENOENT') {
+      throw new Error('Cannot load the database');
+    }
+  }
+}
+
+module.exports = countStudents;
