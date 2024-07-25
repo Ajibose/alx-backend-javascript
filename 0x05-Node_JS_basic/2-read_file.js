5eee7661e083acbe8af7165bfc5303c325972240
Ajibose Ibrahim <ajiboseibrahim12@gmail.com>
2024-07-25 16:35:18 +0100

    Task2

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -2,18 +2,22 @@ const fs = require('node:fs');
 
 function countStudents(path) {
   try {
+    let length = 0
     const contents = fs.readFileSync(path, 'utf8');
     const fields = {};
-    const contentList = contents.trimEnd().toString().split('\n').slice(1);
+    const contentList = contents.split('\n').slice(1);
     for (const content of contentList) {
-      const [firstName, , , field] = content.split(',');
-      if (!(field in fields)) {
-        fields[field] = [];
+      if (content) {
+        length += 1
+        const [firstName, , , field] = content.split(',');
+        if (!(field in fields)) {
+          fields[field] = [];
+        }
+        fields[field].push(firstName);
       }
-      fields[field].push(firstName);
     }
     const noOfStudents = contentList.length;
-    console.log(`Number of students: ${noOfStudents}`);
+    console.log(`Number of students: ${length}`);
     Object.entries(fields).forEach(([key, value]) => {
       console.log(`Number of students in ${key}: ${value.length}. List: ${value.join(', ')}`);
     });
