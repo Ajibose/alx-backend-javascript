daf7ef24d4e37f7b661df2359c0f0fb7dfc18d5e
Ajibose Ibrahim <ajiboseibrahim12@gmail.com>
2024-07-25 16:21:39 +0100

    Task2

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -15,7 +15,7 @@ function countStudents(path) {
     const noOfStudents = contentList.length;
     console.log(`Number of students: ${noOfStudents}`);
     Object.entries(fields).forEach(([key, value]) => {
-      console.log(`Number of students in ${key}: ${value.length}. List: ${value.join(', ')}`);
+      console.log(`Number of students in ${key}: ${value.length}. List: ${value}`);
     });
   } catch (err) {
     if (err.code === 'ENOENT') {
