abfc347cdf57b9b0b388d5ea5d7d99a3107b626e
Ajibose Ibrahim <ajiboseibrahim12@gmail.com>
2024-07-25 21:08:21 +0100

    Task4 - http module

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -4,6 +4,7 @@ const app = createServer((req, resp) => {
   /* eslint-disable no-param-reassign */
   resp.statusCode = 200;
   resp.setHeader('Content-Type', 'text/plain');
+  //resp.end('Hello Holberton School!');
   resp.end('Hello Holberton School!');
   /* eslint-enable no-param-reassign */
 });
