80caba146bb0ec608dff87d478f169c945c348b7
Ajibose Ibrahim <ajiboseibrahim12@gmail.com>
2024-07-27 22:05:25 +0100

    Task6 - Handling routes in http module

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,16 +1,64 @@
 const { createServer } = require('http');
-const countStudents = require('./3-read_file_async.js');
+const { readFile } = require('fs');
+
+function countStudents(fileName) {
+  const students = {};
+  const fields = {};
+  let length = 0;
+  return new Promise((resolve, reject) => {
+    readFile(fileName, (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        let output = '';
+        const lines = data.toString().split('\n');
+        for (let i = 0; i < lines.length; i += 1) {
+          if (lines[i]) {
+            length += 1;
+            const field = lines[i].toString().split(',');
+            if (Object.prototype.hasOwnProperty.call(students, field[3])) {
+              students[field[3]].push(field[0]);
+            } else {
+              students[field[3]] = [field[0]];
+            }
+            if (Object.prototype.hasOwnProperty.call(fields, field[3])) {
+              fields[field[3]] += 1;
+            } else {
+              fields[field[3]] = 1;
+            }
+          }
+        }
+        const l = length - 1;
+        output += `Number of students: ${l}\n`;
+        for (const [key, value] of Object.entries(fields)) {
+          if (key !== 'field') {
+            output += `Number of students in ${key}: ${value}. `;
+            output += `List: ${students[key].join(', ')}\n`;
+          }
+        }
+        resolve(output);
+      }
+    });
+  });
+}
 
 const app = createServer((req, resp) => {
   /* eslint-disable no-param-reassign */
   resp.writeHead(200, { 'Content-Type': 'text/plain' });
-  if (resp.url === '/') {
-    resp.write('Hello Holberton School!');
-  } else if (resp.url === '/students') {
-    resp.write('This is the list of our students');
-    countStudents(database.csv);
+  if (req.url === '/') {
+    resp.end('Hello Holberton School!');
+  } else if (req.url === '/students') {
+    resp.write('This is the list of our students\n');
+    countStudents(process.argv[2].toString())
+      .then((data) => {
+        const outString = data.slice(0, -1);
+        resp.end(outString);
+      })
+      .catch(() => {
+        resp.statusCode = 404;
+        resp.end('Cannot load the database');
+      });
   }
-  resp.end();
   /* eslint-enable no-param-reassign */
 });
 
